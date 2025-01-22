1a6322e2cc7101fd4c7a5ffbc714e72e1968d5b1
James Baker <jlbaker361@gmail.com>
2025-01-21 22:38:31 -0500

    reward and other stuff

--- gym_rl.py
+++ gym_rl.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.optimize import minimize_scalar
 from stable_baselines3 import PPO
 import time
-from simsopt.field import ToroidalField
+from simsopt.field import ToroidalField, InterpolatedField
 from simsopt.geo import CurveXYZFourier
 from simsopt.util.constants import PROTON_MASS, ELEMENTARY_CHARGE, ONE_EV
 from simsopt.field import trace_particles_starting_on_curve,trace_particles
@@ -18,16 +18,17 @@ from simsopt.field import BiotSavart
 import matplotlib.pyplot as plt 
 
 class MagneticOptimizationEnv(gym.Env):
-    def __init__(self,start_positions:list,n_coils:int,max_fourier_n:int,nozzle_radius:float,radius:float,n_particles:int):
+    def __init__(self,start_positions:list,start_velocities:list,n_coils:int,max_fourier_n:int,nozzle_radius:float,radius:float,n_particles:int):
         super(MagneticOptimizationEnv,self).__init__()
         self.start_positions=start_positions #places where we might start a particle
+        self.start_velocities=start_velocities
         self.n_coils=n_coils #how many coils
         self.max_fourier_n=max_fourier_n
         self.nozzle_radius=nozzle_radius
         self.radius=radius
-        self.n_particles-n_particles
+        self.n_particles=n_particles
 
-        parameters_per_coil=1+(2*max_fourier_n)+2 #constant + cos,sin for each mode + z + current
+        self.parameters_per_coil=2+(2*max_fourier_n)+2 #constant x,y + cos,sin for each mode x,y + z + current
         upper_limits_per_coil=[1.0]+[1.0 for _ in range(2*max_fourier_n)] +[1.0]+[10000]
         upper_limits=np.concatenate([upper_limits_per_coil for _ in range(n_coils)])
         lower_limits_per_coil=[0.]+[0.0 for _ in range(2*max_fourier_n)]+[0.]+[100]
@@ -43,12 +44,31 @@ class MagneticOptimizationEnv(gym.Env):
         )
 
 
-    def calculate_loss(self,observation:list):
-        return
+    def calculate_reward(self,observation:list):
+        reward=0.0
+        for [x,y,z,v_x,v_y,v_z] in observation:
+            if np.linalg.norm([x,y])< self.nozzle_radius:
+                reward+=v_z #for each particle, that is in the nozzle, we want as much z momentumas possible
     
     def step(self,action):
 
         #action=np.clip(action, [0 for _ in range(18)], self.upper_limits)
+        #calculate particle locations
+        coil_list=[]
+        for c in range(self.n_coils):
+            coil_parameters=action[c:c+self.parameters_per_coil]
+            curve = CurveXYZFourier(1000, self.max_fourier_n)  # 100 = Number of quadrature points, 1 = max Fourier mode number
+            fourier_amplitudes=coil_parameters[:-2]
+            curve.x = fourier_amplitudes+ coil_parameters[-2]+[0. for _ in range(2*self.max_fourier_n)]  # Set Fourier amplitudes
+            coil = Coil(curve, Current(coil_parameters[-1]))  # 10 kAmpere-turns
+            coil_list.append(coil)
+
+        field=BiotSavart([coil_list])
+        field=InterpolatedField(field)
+        m = PROTON_MASS
+        q = ELEMENTARY_CHARGE
+        res_tys,res_phi_hits=trace_particles(field,self.start_positions,self.start_velocities,mass=m,charge=q)
+
         
         return #observation, reward, terminated, truncated, info
 
