0f6282dfda06584fd746640c5f59bef4822fb034
James Baker <jlbaker361@gmail.com>
2025-01-23 11:00:23 -0500

    typos

--- diffusion_based.py
+++ diffusion_based.py
@@ -33,6 +33,11 @@ parser.add_argument("--epochs",type=int,default=10)
 parser.add_argument("--n_layers",type=int,default=4)
 parser.add_argument("--residuals",action="store_true")
 parser.add_argument("--increasing",action="store_true")
+parser.add_argument("--batch_size",type=int,default=4)
+parser.add_argument("--batches_per_epoch",type=int,default=20)
+parser.add_argument("--denoise_steps",type=int,default=10)
+parser.add_argument("--gradient_accumulation_steps",type=int,default=2)
+parser.add_argument("--nozzle_radius",type=float,default=0.1)
 
 
 def calculate_reward(observation:list,nozzle_radius:int):
@@ -45,29 +50,8 @@ def calculate_reward(observation:list,nozzle_radius:int):
     #print("found rewards")
     return reward,counts
 
-def evaluate_fourier(fourier_coefficients:list,
-                     max_fourier_mode:int,
-                     start_positions:list,
-                     start_velocities:list,
-                     stopping_criteria:list,
-                     nozzle_radius:float):
-    n_coils=len(fourier_coefficients)
-    coil_list=[]
-    for f_c in fourier_coefficients:
-        curve = CurveXYZFourier(1000, max_fourier_mode)
-        all_fourier=np.concatenate(f_c, [0 for _ in range(2*max_fourier_mode)-1])
-        curve.x=all_fourier
-        coil = Coil(curve, Current(AMPS)) 
-        coil_list.append(coil)
 
-    field=BiotSavart(coil_list)
-    res_tys,res_phi_hits=trace_particles(field,start_positions,start_velocities,mass=m,charge=q,Ekin=Ekin,mode="full",forget_exact_path=True,
-                                                stopping_criteria=stopping_criteria)
-    
-    observation=[rt[-1][1:] for rt in res_tys]
-    reward,counts=calculate_reward(observation,nozzle_radius)
-
-    return reward
+        
 
 class Denoiser(torch.nn.Module):
     def __init__(self, n_features:int, n_layers:int,residuals:bool,increasing:bool):
@@ -85,14 +69,15 @@ class Denoiser(torch.nn.Module):
                 current=prev+diff
             else:
                 current=prev-diff
-            down_layer_list.append(Linear(prev,current))
+            
+            down_layer_list.append(Linear(int(prev),int(current)))
             prev=current
         
         if increasing:
-            self.mid_block=Linear(prev,n_features*2)
+            self.mid_block=Linear(int(prev),int(n_features*2))
             prev=n_features*2
         else:
-            self.mid_block=Linear(prev,n_features//2)
+            self.mid_block=Linear(int(prev),int(n_features//2))
             prev=n_features//2
         
 
@@ -101,7 +86,7 @@ class Denoiser(torch.nn.Module):
                 current=prev-diff
             else:
                 current=prev+diff
-            up_layer_list.append(Linear(prev,current))
+            up_layer_list.append(Linear(int(prev),int(current)))
             prev=current
 
         self.down_block=Sequential(*down_layer_list)
@@ -117,7 +102,8 @@ class Denoiser(torch.nn.Module):
             residual_list=residual_list[::-1]
             print("len res list",len(residual_list))
             for i,linear in enumerate(self.up_block):
-                x=linear(x)+residual_list[i]
+                
+                x=linear(x +residual_list[i])
         else:
             x=self.down_block(x)
             x=self.mid_block(x)
@@ -128,12 +114,59 @@ class Denoiser(torch.nn.Module):
 
 
 def main(args):
+    max_fourier_mode=args.max_fourier_mode
+    start_positions=[[0,0,0]]
+    start_velocities=[[1]]
+    stopping_criteria=[MaxRStoppingCriterion(1),MinZStoppingCriterion(0),MaxZStoppingCriterion(1)]
+    nozzle_radius=args.nozzle_radius
+
+
+    def evaluate_fourier(fourier_coefficients:list):
+        n_coils=len(fourier_coefficients)
+        coil_list=[]
+        for f_c in fourier_coefficients:
+            curve = CurveXYZFourier(1000, max_fourier_mode)
+            all_fourier=np.concatenate(f_c, [0 for _ in range(2*max_fourier_mode -1)])
+            curve.x=all_fourier
+            coil = Coil(curve, Current(AMPS)) 
+            coil_list.append(coil)
+
+        field=BiotSavart(coil_list)
+        res_tys,res_phi_hits=trace_particles(field,start_positions,start_velocities,mass=m,charge=q,Ekin=Ekin,mode="full",forget_exact_path=True,
+                                                    stopping_criteria=stopping_criteria)
+        
+        observation=[rt[-1][1:] for rt in res_tys]
+        reward,counts=calculate_reward(observation,nozzle_radius)
+
+        return reward
+
+    class FourierReward(torch.nn.Module):
+        def forward(self,x_list:list):
+            rewards=[evaluate_fourier(x) for x in x_list]
+            return -rewards
+        
+    
     n_features=args.n_coils+args.n_coils*(1+2*args.max_fourier_mode)
     denoiser=Denoiser(n_features,args.n_layers,args.residuals,args.increasing)
     model_parameters=[p for p in denoiser.parameters()]
     print(f"optimzing {len(model_parameters)} model params")
-    optimizer=torch.optim.adamw.AdamW(model_parameters)
+    optimizer=torch.optim.AdamW(model_parameters)
     denoiser(torch.randn((1,n_features)))
+    reward_module=FourierReward()
+
+    for e in range(args.epochs):
+        for b in range(args.batches_per_epoch):
+            optimizer.zero_grad()
+            noise=torch.randn((args.batch_size,n_features))
+            for s in range(args.denoise_steps):
+                noise=denoiser(noise)
+            loss=reward_module(noise)
+
+            loss.backward()
+            optimizer.step()
+
+
+        
 
 if __name__=="__main__":
     args=parser.parse_args()
